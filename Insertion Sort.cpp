e0049259123b49d52337644cc78ddd32a3eb5578
Philippe Adams <33237026+adamsphilippe@users.noreply.github.com>
2018-04-11 18:14:19 -0300

    Add files via upload

---
+++ Insertion Sort.cpp
@@ -0,0 +1,49 @@
+/*
+Aluno: Philippe Adams
+Matrícula; 3214142
+*/
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+// função que executa a ordenação do Insertion Sort.
+void insertionSortC(int array[], int tamanho)
+{
+      int i, j, chave;
+
+      for (i = 1; i < tamanho; i++)
+      {
+            j = i;
+            while (j > 0 && array[j - 1] > array[j])
+            {
+                  chave = array[j];
+                  array[j] = array[j - 1];
+                  array[j - 1] = chave;
+                  j--;
+            }
+      }
+}
+
+int main(int argc, char** argv)
+{
+	int array[100], tamanho, ordem;
+
+	printf("\n\t Defina o numero de termos: ");
+	scanf("%d", &tamanho);
+
+	printf("\n\t Digite os numeros do arranjo: ");
+	for(int i = 0; i < tamanho;i++)
+    {
+        cin >> array[i];
+	}
+
+    insertionSortC(array, tamanho);
+    printf("\n\t Numeros ordenados em ordem crescente: ");
+		for(int i=0; i<tamanho; i++)
+        {
+            printf("%d ", array[i]);
+        }
+
+	return 0;
+}
---
+++ Selection Sort.c
@@ -0,0 +1,52 @@
+/*
+Aluno:
+Matrícula;
+*/
+
+#include <stdio.h>
+
+void selection_sort (int vetor[],int max) {
+  int i, j, min, aux;
+
+  for (i = 0; i < (max - 1); i++) {
+    /* O minimo é o primeiro número não ordenado ainda */
+    min = i;
+    for (j = i+1; j < max; j++) {
+      /* Caso tenha algum numero menor ele faz a troca do minimo*/
+      if (vetor[j] < vetor[min]) {
+	min = j;
+      }
+    }
+    /* Se o minimo for diferente do primeiro numero não ordenado ele faz a troca para ordena-los*/
+    if (i != min) {
+      aux = vetor[i];
+      vetor[i] = vetor[min];
+      vetor[min] = aux;
+    }
+  }
+  /* Imprime o vetor ordenado */
+  printf("\n\t Numeros ordenados em ordem crescente: ");
+  for (i = 0; i < max; i++) {
+    printf ("%d ",vetor[i]);
+  }
+  printf ("\n");
+}
+
+int main () {
+  int max, i;
+  //int vetor[100];
+  int  vetor[100];
+  /* Lê o máximo de algarismos do vetor*/
+  printf("\n\t Defina o numero de termos: ");
+	scanf ("%d",&max);
+
+  /* Lê os algarismos do vetor */
+  printf("\n\t Digite os numeros do arranjo: ");
+	for(int i = 0; i < max; i++)
+    {
+        scanf ("%d",&vetor[i]);
+	}
+
+  selection_sort (vetor, max);
+
+}
